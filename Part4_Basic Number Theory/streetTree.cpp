c80bb28bd53a3208ecc169ae7ee0fa4e98d48387
invictus89 <1nvictus@naver.com>
2019-02-06 18:11:03 +0900

    완료(1차)

---
+++ Part4_Basic Number Theory/streetTree.cpp
@@ -0,0 +1,70 @@
+﻿// 문제 : streetTree
+// 가로수의 위치가 주어질 때 가로수의 최소개수를 출력하라
+
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+
+
+long fGcd(long num1, long num2){
+
+  if(num1 % num2 == 0) return num2;
+
+  else return fGcd(num2, num1 % num2);
+
+}
+
+
+
+int main() {
+
+  int arr[100000];
+
+  //Please Enter Your Code Here
+
+  int n;
+
+  cin >> n;
+
+  for(int i = 0; i < n; i++)
+
+    cin >> arr[i];
+
+  
+
+  int preValue = arr[1] - arr[0];
+
+  int curValue = 0;
+
+  for(int i = 1; i < n; i++){
+
+    int curValue = arr[i] - arr[i-1];
+
+    preValue = (preValue < curValue) ? fGcd(curValue, preValue) : fGcd(preValue, curValue);
+
+    //cout << curValue << ' ' << preValue << endl;
+
+  }
+
+  
+
+  
+
+  int count = 0;
+
+  for(int i = 1; i < n; i++){
+
+    count += ((arr[i] - arr[i-1]) / preValue) - 1;
+
+  }
+
+  
+
+  cout << count;
+
+  return 0;
+
+}
