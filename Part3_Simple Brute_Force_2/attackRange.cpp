9154260370b3f2acdc349ff4c57c548fda612283
invictus89 <1nvictus@naver.com>
2019-02-06 17:29:55 +0900

    Loop를 활용한 간단한 완전 탐색2

---
+++ Part3_Simple Brute_Force_2/attackRange.cpp
@@ -0,0 +1,102 @@
+﻿//문제 : 면적 합 구하기
+//도형마다 값을 채워나간다. 완전 탐색하며 값이 0이 아닌 배열에 닿으면 카운트를 1증가시킨다.
+
+#include <stdio.h>
+
+#include <iostream>
+
+#include <limits.h>
+
+#include <math.h>
+
+using namespace std;
+
+#define SIZE 100
+
+int main() {
+
+
+
+  //Please Enter Your Code Here
+
+  int x1, y1, x2, y2;
+
+  int i1, j1, i2, j2;
+
+  int maxValue = INT_MIN;
+
+  int arr[100][100] = {0};
+
+  int count = 0;
+
+
+
+  for(int n = 0; n < 4; n++){
+
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    i1 = (SIZE - 1) - y1;
+
+    j1= x1;
+
+    i2 = (SIZE -1) - (y2 - 1);
+
+    j2 = x2 - 1;
+
+    
+
+    //cout << i1 << ' ' << j1 << ' ' << i2 << ' ' << j2 << endl;
+
+    
+
+    for(int t = i2; t <= i1; t++){
+
+      for(int k = j1; k <= j2; k++ ){
+
+        arr[t][k] = n + 1;
+
+      }
+
+    }
+
+  }
+
+  
+
+  /*
+
+  for(int i = 0; i < SIZE; i++){
+
+    for(int j = 0; j < SIZE; j++){
+
+      cout << arr[i][j] << ' ';
+
+    }
+
+    cout << endl;
+
+  }
+
+  */
+
+    
+
+  for(int i = 0; i < SIZE; i++){
+
+    for(int j = 0; j < SIZE; j++){
+
+      if(arr[i][j] != 0) count++;
+
+    }
+
+  }
+
+  
+
+  cout << count << endl;
+
+  
+
+  return 0;
+
+}
---
+++ Part3_Simple Brute_Force_2/seat.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+
+//문제 : 달팽이 배열 채우기
+//루프 대신 재귀함수를 사용하여 풀어 볼 것  
+//Please Enter Your Code Here
+
+  int c, r, k;
+
+  cin >> c >> r >> k;
+
+  int arr[1000][1000];
+
+  int num = 1;
+
+  int directionX = -1;
+
+  int directionY = 1;
+
+  int posX = r;
+
+  int posY = 0;
+
+  int roofX = r;
+
+  int roofY = c - 1;
+
+  bool result = false;
+
+  
+
+  while(1){
+
+    
+
+     for(int i = 0; i < roofX; i++){
+
+        posX = posX + directionX;
+
+        arr[posX][posY] = num;
+
+        if(num == k) {result = true; break;}
+
+        num++;
+
+      }
+
+      roofX--;
+
+      directionX = -directionX;
+
+    if(roofY == 0 || result == true) break;
+
+  
+
+    for(int i = 0; i < roofY; i++){
+
+      posY = posY + directionY;
+
+      arr[posX][posY] = num;
+
+      if(num == k) {result = true; break;}
+
+      num++;
+
+    }
+
+    roofY--;
+
+    directionY = -directionY;
+
+    if(roofX == 0 || result == true) break;
+
+  }
+
+  //cout << "posX : " << posX << ' ' << "PosY : " << posY << endl;
+
+  if(result == 0) cout << 0 << endl;
+
+  else cout << posY + 1 << " " << r - posX << endl;
+
+
+
+  
+
+  return 0;
+
+}
