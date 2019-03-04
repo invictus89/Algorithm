46204220576d2b9133cbe4edba6b71c66ba28624
invictus89 <1nvictus@naver.com>
2019-03-04 14:07:02 +0900

    Add chapter 10. BinarySearch

---
+++ Part9_Binary Search/2차식 정답 추측.cpp
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+using namespace std;
+
+/*
+
+2차식 정답 추측
+문제
+2차식 f(x) = x*x+ x 가 있고, 숫자 a가 주어진다. 
+우리는 f(x) = a 를 만족하는 x의 값을 찾고 싶지만, 보통 이 값은 정수로 떨어지지 않는 경우가 많다. 
+예를 들어, f(x) = 20 을 풀고자 한다면, x = 4이기 때문에 이는 정수이지만, f(x) = 103 을 풀고자 한다면 이는 x = 9.6612... 로써 정수가 아니다.
+
+이 문제에서는 x의 정수부분이 얼마인지를 구하는 프로그램을 작성하시오. 
+예를 들어, f(x) = 103 을 풀고자 한다면, x = 9.6612... 이기 때문에 정수부분은 9가 된다.
+
+
+
+입력
+첫 번째 줄에 숫자 a가 주어진다. ( 1 ≤ a ≤ 1,000,000,000,000,000,000 )
+
+출력
+f(x) = a 를 만족하는 x의 정수부분을 출력한다.
+
+예제 입력
+103
+예제 출력
+9
+
+
+
+
+*/
+long long cal(long num) {
+	return num * num + num;
+}
+
+long binarySearch(long start, long end, long target) {
+	//start ~ end 까지 이진 탐색으로 f(x) = x^2 + x의 해 구하기
+	//printf("s:%d, e:%d\n", start, end);
+
+	if (start >= end) {
+		//기본 기저 조건
+		int answer = (cal(start) > target) ? start - 1 : start;
+		return answer;
+	}
+	else {
+		long mid = (start + end) / 2;
+		long val = cal(mid);
+
+		if (target == val) return mid;
+		else if (target > val) {
+			return binarySearch(mid + 1, end, target);
+		}
+		else {
+			return binarySearch(start, mid - 1, target);
+		}
+	}
+}
+
+int main() {
+
+	//Please Enter Your Code Here
+	long long a;
+	cin >> a;
+
+	//cout << a;
+	long sq_max_long = sqrt(LONG_MAX);
+	//탐색 리스트 범위
+
+	long answer = binarySearch(0, sq_max_long, a);
+	cout << answer;
+	return 0;
+}
---
+++ Part9_Binary Search/숫자박스.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+using namespace std;
+#define SIZE 100000
+
+/*
+
+
+첫째 줄에 윤성이가 가지고 있는 숫자 정보의 개수 N (1 ≤ N ≤ 300,000)이가 주어진다. 둘째 줄에는 숫자 정보들이 주어진다. 숫자는 1,000,000이하의 정수이다. 두 숫자 카드에 같은 숫자가 적혀있는 경우는 없다.
+
+셋째 줄에는 M (1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 윤성이가 숫자 박스에 있는지 아닌지를 구해야 할 M개의 숫자가 주어지며, 이 숫자는 공백으로 구분되어져 있다. 이숫자도 1,000,000이하의 정수이다.
+
+출력
+첫째 줄에 입력으로 주어진 M개의 숫자에 대해서, 각 숫자가 숫자 상자안에 있으면 1, 아니면 0을 개행으로 구분해 출력한다.
+
+
+
+예제 입력
+5
+6 3 2 10 -10
+8
+10 9 -5 2 3 4 5 -10
+예제 출력
+1
+0
+0
+1
+1
+0
+0
+1
+
+
+*/
+void merging(int arr[], int start1, int end1, int start2, int end2){
+  //arr함수의 왼쪽(start1 ~ end1)과 오른쪽(start2 ~ end2)까지를
+  //합친다.
+  
+  int temp[SIZE];
+  int tempIndex = 0;
+  int q = start1;
+  int p = start2;
+  
+  //1 3 4 6   2 5 6 7
+  while(q <= end1 && p  <= end2){
+    if(arr[q] > arr[p]){
+      temp[tempIndex++] = arr[p];
+      p++;
+    }else{
+      temp[tempIndex++] = arr[q];
+      q++;
+    }
+  }
+  
+  
+  if(q > end1){
+    //왼쪽 값을 모두 넣어
+    //오른쪽 값의 나머지를 배열에 넣는 경우
+    for(int i = p; i <= end2; i++){
+      temp[tempIndex++] = arr[i];
+    }
+  }
+  
+  if(p > end2){
+    //오른쪽 값을 먼저 모두 넣어
+    //왼쪽 값의 나머지를 배열에 넣는 경우
+    for(int i = q; i <= end1; i++){
+      temp[tempIndex++] = arr[i];
+    }
+  }
+  
+  for(int i = start1; i <= end2; i++){
+    //temp배열 값을 arr로 다시 넣기
+    arr[i] = temp[i-start1];
+  }
+  
+}
+void mergeSort(int arr[], int start, int end){
+  //arr배열의 start에서 end까지 합병정렬하는 함수
+  
+  if(start >= end){
+    //기저조건 : start가 end를 넘으면 종료
+    return;
+  }else{
+    //구체적 함수 정의
+    
+    int mid = (start+end)/2;
+    //Ex. start = 2, end = 4
+    // 2 3   4
+    
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid+1, end);
+    merging(arr, start, mid, mid+1, end);
+    //왼쪽(start~mid)과 오른쪽(mid+1 ~ end)까지를 합친다.
+  }
+  
+}
+
+int main(){
+  int arr[SIZE];
+  int n;
+  cin >> n;
+  
+  for(int i = 0; i < SIZE; i++){
+    cin >> arr[i];
+  }
+  
+  //재귀 호출
+  mergeSort(arr, 0, n-1);
+  
+  for(int i = 0; i < n; i++){
+    cout << arr[i] << ' ';
+  }
+  
+}
