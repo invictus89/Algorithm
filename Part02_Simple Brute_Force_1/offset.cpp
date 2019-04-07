62c40b67dcc3526dd24499d5b4451885d1b1e705
Baek, Sang-Woo <47375763+invictus89@users.noreply.github.com>
2019-04-07 19:02:28 +0900

    Re Re

--- Part02_Simple Brute_Force_1/offset.cpp
+++ Part02_Simple Brute_Force_1/offset.cpp
@@ -1,90 +1,51 @@
-﻿/*
-문제 : offset
-*/
 #include <stdio.h>
-
 #include <iostream>
-
 using namespace std;
-
-
-
+const int MAX = 7;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
 
 int main() {
 
-
-
   //Please Enter Your Code Here
-
+  int arr[MAX][MAX];
   
-
-  bool okStar = true;
-
-  char arr[5][5];
-
-  for(int i = 0; i < 5; i++){
-
-    for(int j = 0; j < 5; j++){
-
-      cin >> arr[i][j];
-
+  for(int i = 0; i < MAX; i++){
+    for(int j = 0; j < MAX; j++){
+      arr[i][j] = 10;
     }
-
   }
-
-  for(int i = 0; i < 5; i++){
-
-    for(int j = 0; j < 5; j++){
-
-      okStar = true;
-
-      if(i-1 >= 0){
-
-        if(arr[i][j] >= arr[i-1][j]) okStar = false;
-
-      }
-
-      if(i+1 < 5){
-
-        if(arr[i][j] >= arr[i+1][j]) okStar = false;
-
-      }
-
-      if(j-1 >= 0){
-
-        if(arr[i][j] >= arr[i][j-1]) okStar = false;
-
-      }
-
-      if(j+1 < 5){
-
-        if(arr[i][j] >= arr[i][j+1]) okStar = false;
-
-      }
-
-      if(okStar == true) arr[i][j] = '*';
-
-    }
-
-  }
-
   
-
-    for(int i = 0; i < 5; i++){
-
-      for(int j = 0; j < 5; j++){
-
-        cout << arr[i][j] << ' ';
-
+   for(int i = 1; i < MAX-1; i++){
+    for(int j = 1; j < MAX-1; j++){
+      scanf("%d", &arr[i][j]);
     }
-
-    cout << endl;
-
   }
-
   
-
+  for(int i = 1; i < MAX-1; i++){
+    for(int j = 1; j < MAX-1; j++){
+      bool check = true;
+      for(int k = 0; k < 4; k++){
+        int nx = i + dx[k];
+        int ny = j + dy[k];
+        
+        if(arr[i][j] < arr[nx][ny]){
+          check = true;
+        }else{
+          check = false;
+          break;
+        }
+      }
+      if(check == true){
+        printf("%c ", '*');
+      }else{
+        printf("%d ", arr[i][j]);
+      }
+      
+    }
+    printf("\n");
+  }
+  
   return 0;
-
 }
