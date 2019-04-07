961de4dc4389e813c96e64535422b9760c506a7e
Baek, Sang-Woo <47375763+invictus89@users.noreply.github.com>
2019-04-07 19:01:47 +0900

    Re Re

--- Part02_Simple Brute_Force_1/mine.cpp
+++ Part02_Simple Brute_Force_1/mine.cpp
@@ -1,4 +1,53 @@
 ﻿//문제 : mine
+
+
+
+//Second Solution
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+const int MAX = 110;
+
+int dx[] = {1, -1, 0, 0, 1, -1, 1, -1};
+int dy[] = {0, 0, 1, -1, -1, 1, 1, -1};
+int arr[MAX][MAX];
+int N, M;
+int X, Y;
+
+int main() {
+
+  //Please Enter Your Code Here
+  scanf("%d %d %d %d", &N, &M, &X, &Y);
+  
+  for(int i = 0; i < N+2; i++){
+    for(int j = 0; j < M+2; j++){
+      arr[i][j] = 9;
+    }
+  }
+  
+  for(int i = 1; i < N+1; i++){
+    for(int j = 1; j < M+1; j++){
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  
+  int count = 0;
+  
+  if(arr[X][Y] == 1){
+    printf("game over");
+    return 0;
+  }else{
+    for(int i = 0; i < 8; i++){
+      int nx = X + dx[i];
+      int ny = Y + dy[i];
+      
+      if(arr[nx][ny] == 1) count++;
+    }
+  }
+  printf("%d", count);
+  return 0;
+}
+//Frist Solution
 #include <stdio.h>
 
 #include <iostream>
@@ -53,33 +102,9 @@ int main() {
     }  
 
      cout << count << endl;
-
   }
-
-  
-
-
-
-
-
-  /*
-
-  for(int i = 0; i < n; i ++){
-
-    for(int j = 0; j < m; j++){
-
-      cout << arr[i][j] << ' ';
-
-    }
-
-    cout << endl;
-
-  }
-
-  */
-
-  
 
   return 0;
 
 }
+
