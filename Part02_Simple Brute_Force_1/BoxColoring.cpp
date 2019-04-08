1c411602dc72461f938bfd62c1eb18324a421f31
Baek, Sang-Woo <47375763+invictus89@users.noreply.github.com>
2019-04-08 22:55:38 +0900

    Re Re

--- Part02_Simple Brute_Force_1/BoxColoring.cpp
+++ Part02_Simple Brute_Force_1/BoxColoring.cpp
@@ -1,7 +1,44 @@
-﻿/*
-문제 : 상자 색칠
+/*
+한 색깔 당 사용가능한 횟수와 그 합을 생각해 볼 것
+
 */
 
+//두번째 푼 방법
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+const int MAX = 1010;
+
+int N;
+int arr[MAX];
+int cnt[MAX];
+
+int main() {
+  scanf("%d", &N);
+  for(int i = 0; i < N; i++) scanf("%d", &arr[i]);
+  
+  for(int i = 0; i < N; i++){
+    if((++cnt[arr[i]]) >= 2) cnt[arr[i]] = 2; 
+  }
+
+  int countZeroNot = 0;
+  int sum = 0;
+  for(int i = 0; i < N; i++){
+    sum += cnt[i];
+    if(cnt[arr[i]] != 0) countZeroNot++;
+  }
+  
+  if(countZeroNot >= 3 && sum >= 6) {
+    printf("YES");
+    return 0;
+  }
+  
+  printf("NO");
+  
+  return 0;
+}
+
+//첫번째 푼 방법
 #include <stdio.h>
 #include <iostream>
 #include <algorithm>
